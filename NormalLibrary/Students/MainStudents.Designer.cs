7bfe925d2fdd5587ca5c42b5902d98d06da15a87
oguri <oguri.takeshi39@gmail.com>
2022-10-03 23:15:08 +0700

    Basic layout for home

--- NormalLibrary/Students/MainStudents.Designer.cs
+++ NormalLibrary/Students/MainStudents.Designer.cs
@@ -34,16 +34,35 @@
             this.btnExit = new System.Windows.Forms.Button();
             this.panelBottom = new System.Windows.Forms.Panel();
             this.label5 = new System.Windows.Forms.Label();
-            this.panelLeftMenu = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panelLeftMenu = new System.Windows.Forms.FlowLayoutPanel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.homeTop.SuspendLayout();
             this.panelBottom.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panelLeftMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // homeTop
             // 
             this.homeTop.BackColor = System.Drawing.Color.Transparent;
+            this.homeTop.Controls.Add(this.panel2);
+            this.homeTop.Controls.Add(this.panel1);
             this.homeTop.Controls.Add(this.label1);
             this.homeTop.Controls.Add(this.btnMinimize);
             this.homeTop.Controls.Add(this.btnResize);
@@ -51,7 +70,7 @@
             this.homeTop.Dock = System.Windows.Forms.DockStyle.Top;
             this.homeTop.Location = new System.Drawing.Point(0, 0);
             this.homeTop.Name = "homeTop";
-            this.homeTop.Size = new System.Drawing.Size(1250, 25);
+            this.homeTop.Size = new System.Drawing.Size(1250, 26);
             this.homeTop.TabIndex = 2;
             this.homeTop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.homeTop_MouseDown);
             // 
@@ -121,22 +140,6 @@
             this.label5.TabIndex = 9;
             this.label5.Text = "Design by Oguri Takeshi";
             // 
-            // panelLeftMenu
-            // 
-            this.panelLeftMenu.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelLeftMenu.Location = new System.Drawing.Point(0, 25);
-            this.panelLeftMenu.Name = "panelLeftMenu";
-            this.panelLeftMenu.Size = new System.Drawing.Size(200, 721);
-            this.panelLeftMenu.TabIndex = 4;
-            // 
-            // panel1
-            // 
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(200, 25);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1050, 721);
-            this.panel1.TabIndex = 5;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -147,13 +150,170 @@
             this.label1.TabIndex = 10;
             this.label1.Text = "Library";
             // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1250, 1);
+            this.panel1.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 24);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1250, 1);
+            this.panel2.TabIndex = 12;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel3.Location = new System.Drawing.Point(1050, 26);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 720);
+            this.panel3.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label6);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 100);
+            this.panel4.TabIndex = 0;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label6.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(35, 26);
+            this.label6.Margin = new System.Windows.Forms.Padding(10);
+            this.label6.Name = "label6";
+            this.label6.Padding = new System.Windows.Forms.Padding(10);
+            this.label6.Size = new System.Drawing.Size(136, 48);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "New books";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Black;
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel5.Location = new System.Drawing.Point(150, 26);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(1, 720);
+            this.panel5.TabIndex = 6;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.label7);
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel7.Location = new System.Drawing.Point(0, 428);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(147, 85);
+            this.panel7.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label7.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(10, 24);
+            this.label7.Margin = new System.Windows.Forms.Padding(10);
+            this.label7.Name = "label7";
+            this.label7.Padding = new System.Windows.Forms.Padding(10);
+            this.label7.Size = new System.Drawing.Size(94, 48);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Profile";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.panel7);
+            this.panel6.Location = new System.Drawing.Point(3, 207);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(147, 513);
+            this.panel6.TabIndex = 15;
+            // 
+            // panel8
+            // 
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel8.Location = new System.Drawing.Point(151, 26);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(899, 100);
+            this.panel8.TabIndex = 7;
+            // 
+            // panelLeftMenu
+            // 
+            this.panelLeftMenu.Controls.Add(this.label3);
+            this.panelLeftMenu.Controls.Add(this.label2);
+            this.panelLeftMenu.Controls.Add(this.label4);
+            this.panelLeftMenu.Controls.Add(this.panel6);
+            this.panelLeftMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeftMenu.Location = new System.Drawing.Point(0, 26);
+            this.panelLeftMenu.MaximumSize = new System.Drawing.Size(150, 0);
+            this.panelLeftMenu.Name = "panelLeftMenu";
+            this.panelLeftMenu.Size = new System.Drawing.Size(150, 720);
+            this.panelLeftMenu.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 10);
+            this.label3.Margin = new System.Windows.Forms.Padding(10);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(10);
+            this.label3.Size = new System.Drawing.Size(86, 48);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Home";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(10);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(10);
+            this.label2.Size = new System.Drawing.Size(109, 48);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Bookself";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("MV Boli", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(10, 146);
+            this.label4.Margin = new System.Windows.Forms.Padding(10);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(10);
+            this.label4.Size = new System.Drawing.Size(109, 48);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Contact";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // MainStudents
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Gainsboro;
             this.ClientSize = new System.Drawing.Size(1250, 768);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panelLeftMenu);
             this.Controls.Add(this.panelBottom);
             this.Controls.Add(this.homeTop);
@@ -165,6 +325,14 @@
             this.homeTop.PerformLayout();
             this.panelBottom.ResumeLayout(false);
             this.panelBottom.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panelLeftMenu.ResumeLayout(false);
+            this.panelLeftMenu.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -177,8 +345,20 @@
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Panel panelBottom;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Panel panelLeftMenu;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.FlowLayoutPanel panelLeftMenu;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
     }
 }
